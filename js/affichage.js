ffb79c576b7707480835ecb80d6286b401843db9
Alex-san <bobbax4@live.fr>
2017-02-28 14:39:10 +0100

    Changement de menu

--- js/affichage.js
+++ js/affichage.js
@@ -12,17 +12,19 @@ $('.navbar li').click(function(e) {
 $('#burgerbutton').click(function(){
 	if($('.navbar').css("margin-left") == 0+"px" && !$('.navbar').is(':animated'))
 	{
-		$('.navbar').animate({"margin-left": '-='+150});
+		$('.navbar').animate({"margin-left": '-='+230});
+		$('.content').css({"filter": "brightness(100%)"})
 
 	}
 	else
 	{
-		        if(!$('.navbar').is(':animated'))//perevent double click to double margin
-		        {
-		        	$('.navbar').animate({"margin-left": '+='+150});
-		        }
-		    }
-		});
+		if(!$('.navbar').is(':animated'))//perevent double click to double margin
+		{
+			$('.navbar').animate({"margin-left": '+='+230});
+			$('.content').css({"filter": "brightness(40%)"})
+		}
+	}
+});
 /* Ici j'handle la modification de taille pour le rendre résponsif*/
 $(window).resize(function() {
 	var width = screen.width;
@@ -38,20 +40,21 @@ function resizepost()
 {
 	var width = screen.width;
 	var navbar = $('.navbar').width();
-	if (navbar <= 100){
+	if (navbar <= 200){
 		$('.post').css( "width", width );
 	}
 	else{
 		$('.post').css( "width", width - navbar );
 	}
 };
+resizepost();
 /* Ici je replace le cadre avec tout le contenu suivant quelle contenu est actif, à chaque modification de taille*/
 function recadragecontent(){
 	var width = screen.width;
 	var navbar = $('.navbar').width();
 	var actif = $('.content .post.actif')[0].id;
 	var multiplier = getmultiplier(actif);
-	if (navbar <= 100){
+	if (navbar <= 200){
 		$('.content').css({"margin-left": (-width * multiplier ) });
 	}
 	else{
@@ -71,13 +74,13 @@ function getmultiplier(id){
 	}
 	return 0;
 };
-/* Ici (x4) je slide de gauche à droite suivant quelle bouton est appuyer ( En réalité je pourrais faire ça plus court, mais c'est plus lisible comme ça*/
+/* Ici (x4) je slide de gauche à droite suivant quel bouton est appuyer ( En réalité je pourrais faire ça plus court, mais c'est plus lisible comme ça*/
 $('.navbar a').click(function(){
 	var id = $(this).data("target");
 	var multiplier = getmultiplier(id);
 	var width = screen.width;
 	var navbar = $('.navbar').width();
-	if (navbar <= 100){
+	if (navbar <= 200){
 		$('.content').animate({"margin-left": (-width*multiplier) });
 	}
 	else{
@@ -86,7 +89,13 @@ $('.navbar a').click(function(){
 	$('.content .post.actif').removeClass('actif');
 	$('#' + id).addClass('actif');
 });
-resizepost();
+$('.content').click(function(){
+	var navbar = $('.navbar').width();
+	if (navbar <= 200 && !$('.navbar').is(':animated') && $('.navbar').css("margin-left") == "0px"){
+		$('.navbar').animate({"margin-left": '-='+230});
+		$('.content').css({"filter": "brightness(100%)"})
+	}
+})
 /*Ici je place des tables pour les contenu*/
 $(function(){
 	$("#content").wrapInner("<table cellspacing='30'><tr>");
